77fb4a9ed793de7b7e25b855d2c2f9f28ed831fd
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-28 20:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ IfElseAberto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a hora");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora >= 8 && hora < 23)
+            {
+                Console.WriteLine("Estamos abertos!");
+            }
+            else
+            {
+                Console.WriteLine("Estamos fechados");
+            }
+
+
+        }
+    }
+}
